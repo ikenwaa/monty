fc755ace9974439ab7947d9148a3a8cf4dbb7f34
ikenwaa <ajohn9353@gmail.com>
2022-12-18 23:18:18 +0100

    Added all files

--- monty.h
+++ monty.h
@@ -1,12 +1,38 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef __MONTY_H__
+#define __MONTY_H__
+
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <ctype.h>
 
-/* data structures */
+
+#define QUEUE 1
+#define STACK 0
+#define DELIM " \n\t\a\b"
+
+/**
+ * globales_s - struct with variable globals
+ * @token2: integer
+ * @fd: points to the previous element of the stack (or queue)
+ * @line_buf: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct globales_s
+{
+	char *token2;
+	FILE *fd;
+	char *line_buf;
+
+} globales_t;
+
+extern globales_t globalvar;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -25,7 +51,7 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -38,27 +64,55 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* opcodes related prototypes */
-void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number);
-void push(stack_t **h, unsigned int line_number, const char *n);
-void pop(stack_t **h, unsigned int line_number);
-void pint(stack_t **h, unsigned int line_number);
-void pall(stack_t **h, unsigned int line_number);
-void swap(stack_t **h, unsigned int line_number);
-void nop(stack_t **h, unsigned int line_number);
+/* main.c */
+int main(int argc, char **argv);
 
-/* calculator prototypes */
-void _add(stack_t **h, unsigned int line_number);
-void _sub(stack_t **h, unsigned int line_number);
-void _mul(stack_t **h, unsigned int line_number);
-void _div(stack_t **h, unsigned int line_number);
-void _mod(stack_t **h, unsigned int line_number);
+/* node functions */
+stack_t *create_node_stackfirst(stack_t **stack, int n);
+stack_t *create_node_stackend(stack_t **stack, int n);
+void free_node_stack(stack_t **stack);
 
-/* doubly linked list related prototypes */
-int add_end_node(stack_t **h, int n);
-void delete_end_node(stack_t **h);
-void free_dlist(stack_t **h);
+/* get_builtin */
+int get_builtin(char *token, stack_t **stack, unsigned int line_number);
+/* builtins functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void div_m(stack_t **stack, unsigned int line_number);
+void mod_m(stack_t **stack, unsigned int line_number);
+void pstr_t(stack_t **stack, unsigned int line_number);
+void rotrl(stack_t **stack, unsigned int line_number);
 
-char *token_line(stack_t **h, int n);
+/*error*/
+void stderr_usage(void);
+void stderr_malloc(void);
+void stderr_fopen(char *fd);
+void stderr_int(unsigned int line_number);
+void stderr_unknown(char *token, unsigned int line_number);
+void div_e(unsigned int line_number);
+void op_e(unsigned int line_number, char *op);
+/* TEST F */
+
+void tokerr(int error_code);
+unsigned int array_len(void);
+char *get_int(int num);
+unsigned int abs_m(int);
+int len_buff_uint(unsigned int num, unsigned int base);
+void full_buff(unsigned int num, unsigned int base, char *buff, int buff_size);
+void pop_e(unsigned int line_number);
+void pint_e(unsigned int line_number);
+void pchar_e(unsigned int line_number, char *msg);
+int _isdigit();
+void free_dlistint(stack_t *stack);
+void free_globalvars(void);
+void rotr(stack_t **stack, unsigned int line_number);
+stack_t *add_dnodeint(stack_t **head, int n);
 
 #endif
